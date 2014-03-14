d6c64918941418d23ddc8f1d82134db2936b2547
Pavel Martynov <geddeon@gmail.com>
2014-03-14 10:23:48 +0300

    2_4 updated

--- solutions/pavel_martynau/2/sources/2_4/main.cpp
+++ solutions/pavel_martynau/2/sources/2_4/main.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <algorithm>
 #include <vector>
 #include <stdint.h>
-
-union uint_char {
-	char c[4];
-	uint32_t i;
-};
 
 struct message_str {
 	uint32_t type;
@@ -16,49 +11,37 @@ struct message_str {
 	char * message;
 };
 
-class Message_class {
-	public:
-		uint32_t type;
-		uint32_t time;
-		uint32_t length;
-		char * message;
-};
+bool compareByTime(const message_str &a, const message_str &b) {
+	return a.time < b.time;
+}
 
 int main() {
-	std::ifstream input_file ( BINARY_DIR "/2.4.in", std::ios::in|std::ios::binary );
+	std::ifstream input_file ( BINARY_DIR "/2.4.in", std::ios::binary );
 	std::vector< message_str > messages;
+	uint32_t cur_time = 0;
 	if(input_file.is_open()){
 		message_str msg;
-		uint_char ui;
-		 while(input_file.read( ui.c, 4 )) {
-			std::cout << input_file.eof() << std::endl;
-			msg.type = ui.i;
-			std::cout << msg.type << std::endl;
-			input_file.read( ui.c, 4 );
-			msg.time = ui.i;
-			std::cout << msg.time << std::endl;
-			input_file.read( ui.c, 4 );
-			msg.length = ui.i;
-			std::cout << msg.length << "--------------" << std::endl;
+		 while( input_file.read( (char*)&msg.type, sizeof( msg.type ) ) ) {
+			input_file.read( (char*)&msg.time, sizeof( msg.time ) );
+			input_file.read( (char*)&msg.length, sizeof( msg.length ) );
 			msg.message = new char[msg.length];
 			input_file.read(msg.message, msg.length);
-			for(size_t j = 0; j < msg.length; j++) {
-				std::cout << msg.message[j];
-			}
-					
-			std::cout << std::endl;
-			std::cout << input_file.eof() << std::endl;
-			messages.push_back( msg );
 
+			if(msg.time > cur_time) cur_time = msg.time;
+			
+			if(msg.type == 1u || msg.type == 2u || msg.type == 3u || msg.type == 4u )
+				if(2 > ( cur_time - msg.time ) )
+					messages.push_back( msg );
 		}
 		input_file.close();
 	}
-	std::cout << "size" << messages.size() << std::endl;
-	for(size_t i = 0; i < messages.size(); i++){
-		std::cout << messages[i].type << " " << messages[i].time << " " << messages[i].length << " ";
-		for(size_t j = 0; j < messages[i].length; j++) {
+
+	std::sort(messages.begin(), messages.end(), compareByTime);
+	
+	for(size_t i = 0; i < messages.size(); i++) {
+		std::cout << messages[i].type << " " << messages[i].time << " "<< messages[i].length << " ";
+		for(size_t j = 0; j < messages[i].length; j++)
 			std::cout << messages[i].message[j];
-		}
 		std::cout << std::endl;
 	}
 
