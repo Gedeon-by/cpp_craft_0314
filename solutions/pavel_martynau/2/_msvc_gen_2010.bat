5371f95fb05e75388c89aee24cccf1accf3f2967
Pavel Martynov <martynov.pavel@gmail.com>
2014-03-14 15:19:43 +0300

    2_4 finished

--- solutions/pavel_martynau/2/_msvc_gen_2010.bat
+++ solutions/pavel_martynau/2/_msvc_gen_2010.bat
@@ -1,6 +1,7 @@
 @echo off
 
 set SOLUTION_NAME=puzzle_2_solution
+set BOOST_ROOT=d:/learning/boost_1_55_0
 
 set BUILD_TYPE=Debug
 if [%1]==[Release] (
@@ -13,7 +14,7 @@ if not exist %BUILD_FOLDER% (
 )
 
 cd %BUILD_FOLDER%
-cmake -DVERBOSE=OFF -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DSOLUTION_NAME=%SOLUTION_NAME% -G "Visual Studio 10" ../ 
+cmake -DBOOST_STAGE_FOLDER_WITH_ADDRESS_MODEL=ON -DVERBOSE=OFF -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DSOLUTION_NAME=%SOLUTION_NAME% -G "Visual Studio 10" ../ 
 cd ../
 
 echo "%BUILD_FOLDER%/%SOLUTION_NAME%.sln" > _start_msvc.bat
--- solutions/pavel_martynau/2/sources/2_4/main.cpp
+++ solutions/pavel_martynau/2/sources/2_4/main.cpp
@@ -1,65 +1,54 @@
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <algorithm>
 #include <vector>
 #include <stdint.h>
 
-union uint_char {
-	char c[4];
-	uint32_t i;
-};
 
 struct message_str {
 	uint32_t type;
 	uint32_t time;
 	uint32_t length;
-	char * message;
+	char *message;
 };
 
-class Message_class {
-	public:
-		uint32_t type;
-		uint32_t time;
-		uint32_t length;
-		char * message;
-};
+bool compareByTime( message_str &a, message_str &b){
+	return b.time > a.time;
+}
 
 int main() {
-	std::ifstream input_file ( BINARY_DIR "/2.4.in", std::ios::in|std::ios::binary );
+	std::ifstream input_file ( BINARY_DIR "/2.4.in", std::ios::binary );
+	std::ofstream output_file ( BINARY_DIR "/2.4.out", std::ios::binary );
 	std::vector< message_str > messages;
 	if(input_file.is_open()){
+		uint32_t current_time = 0;
 		message_str msg;
-		uint_char ui;
-		 while(input_file.read( ui.c, 4 )) {
-			std::cout << input_file.eof() << std::endl;
-			msg.type = ui.i;
-			std::cout << msg.type << std::endl;
-			input_file.read( ui.c, 4 );
-			msg.time = ui.i;
-			std::cout << msg.time << std::endl;
-			input_file.read( ui.c, 4 );
-			msg.length = ui.i;
-			std::cout << msg.length << "--------------" << std::endl;
+		 while( input_file.read( (char*)&msg.type, sizeof( msg.type ) ) ) {	
+			input_file.read( (char*)&msg.time, sizeof( msg.time ) );
+			input_file.read( (char*)&msg.length, sizeof( msg.length ) );
 			msg.message = new char[msg.length];
 			input_file.read(msg.message, msg.length);
-			for(size_t j = 0; j < msg.length; j++) {
-				std::cout << msg.message[j];
-			}
-					
-			std::cout << std::endl;
-			std::cout << input_file.eof() << std::endl;
-			messages.push_back( msg );
 
+			if( msg.time > current_time )
+				current_time = msg.time;
+			
+			if( msg.type == 1u || msg.type == 2u || msg.type == 3u || msg.type == 4u )
+				if ( (current_time - msg.time) < 2u )
+					messages.push_back( msg );
 		}
 		input_file.close();
 	}
-	std::cout << "size" << messages.size() << std::endl;
-	for(size_t i = 0; i < messages.size(); i++){
-		std::cout << messages[i].type << " " << messages[i].time << " " << messages[i].length << " ";
-		for(size_t j = 0; j < messages[i].length; j++) {
-			std::cout << messages[i].message[j];
+
+	std::sort(messages.begin(), messages.end(), compareByTime);
+	
+	if(output_file.is_open()){
+		for(size_t i = 0; i < messages.size(); i++){
+			output_file.write( (char*)&messages[i].type, sizeof(messages[i].type) );
+			output_file.write( (char*)&messages[i].time, sizeof(messages[i].time) );
+			output_file.write( (char*)&messages[i].length, sizeof(messages[i].length) );
+			output_file.write( messages[i].message, messages[i].length );
 		}
-		std::cout << std::endl;
+		output_file.close();
 	}
 
 	return 0;
