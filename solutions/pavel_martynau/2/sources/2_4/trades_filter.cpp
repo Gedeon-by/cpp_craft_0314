abe614908c4663bf69d51b4362c70dc5ec784276
Pavel Martynov <geddeon@gmail.com>
2014-03-19 00:34:20 +0300

    2_4 remake

--- solutions/pavel_martynau/2/sources/2_4/trades_filter.cpp
+++ solutions/pavel_martynau/2/sources/2_4/trades_filter.cpp
@@ -1,5 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <boost/cstdint.hpp>
+#include <market_message.h>
 
 int main()
 {
+	std::ifstream in ( BINARY_DIR "/2.4_example.in", std::ios::binary );
+	std::ofstream out ( BINARY_DIR "/2.4.out", std::ofstream::trunc|std::ios::binary );
+
+	boost::uint32_t current_time = 0;
+
+	while( in ){
+		binary_reader::market_message msg(in);
+
+		if( msg.time() > current_time )
+			current_time = msg.time();
+		
+		std::cout << msg.type() << " " << msg.time() << " " << msg.len() << " " << msg.msg()<< " " << sizeof(msg.msg()) << std::endl;
+
+		if( msg.type() == 1u || msg.type() == 2u || msg.type() == 3u || msg.type() == 4u )
+				if ( (current_time - msg.time()) < 2u ) 
+					if(out.is_open())
+						msg.write(out);
+
+	}
+	in.close();
+	out.close();
+
 }
 
--- solutions/pavel_martynau/2/sources/binary_reader/market_message.cpp
+++ solutions/pavel_martynau/2/sources/binary_reader/market_message.cpp
@@ -3,20 +3,29 @@
 
 binary_reader::market_message::market_message( std::ifstream& in )
 {
-	// your code
+	in.read( (char*)&type_, sizeof( type_ ) );
+	in.read( (char*)&time_, sizeof( time_ ) );
+	in.read( (char*)&len_, sizeof( len_ ) );
+	msg_ = new char[len_];
+	in.read(msg_, len_);
 }
-binary_reader::market_message::market_message( const boost::uint32_t type, const boost::uint32_t time, const char* const msg )
+binary_reader::market_message::market_message( const boost::uint32_t type, const boost::uint32_t time, const boost::uint32_t len, const char* const msg ) : type_(type), time_(time), len_(len)
 {
-	// your code
+		msg_ = new char[len_];
+		strncpy(msg_, msg, len_);
 }
 void binary_reader::market_message::write( std::ofstream& out )
 {
-	// your code
+	out.write( (char*)&type_, sizeof(type_) );
+	out.write( (char*)&time_, sizeof(time_) );
+	out.write( (char*)&len_, sizeof(len_) );
+	out.write( msg_, len_ );
+
 }
 
 binary_reader::market_message::~market_message()
 {
-	// your code
+	delete[] msg_;
 }
 //
 boost::uint32_t binary_reader::market_message::type() const
@@ -26,6 +35,10 @@ boost::uint32_t binary_reader::market_message::type() const
 boost::uint32_t binary_reader::market_message::time() const
 {
 	return time_;
+}
+boost::uint32_t binary_reader::market_message::len() const
+{
+	return len_;
 }
 const char* const binary_reader::market_message::msg() const
 {
